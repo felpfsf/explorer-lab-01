36172b070f2d0febd1ee10270be6af8c1eb141b0
felpfsf <feferreira.sf@gmail.com>
2022-10-18 19:51:03 -0300

    feat: aula 01 alterando attr do cartao

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-import "./css/index.css"
+import './css/index.css'
+
+const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
+const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
+const ccLogo = document.querySelector('.cc-logo > span:nth-child(2) > img')
+
+console.log(ccLogo.src);
+
+function setCardType(cardType) {
+  const colors = {
+    visa: ['#2D57F2', '#436D99'],
+    mastercard: ['#C69347', '#DF6F29'],
+    javascript:['#EFF30A','#CBC88C'],
+    default: ['black', 'gray']
+  }
+  
+  ccBgColor01.setAttribute('fill', colors[cardType][0])
+  ccBgColor02.setAttribute('fill', colors[cardType][1])
+  ccLogo.setAttribute('src', `cc-${cardType}.svg`)
+}
+
+setCardType('javascript')
